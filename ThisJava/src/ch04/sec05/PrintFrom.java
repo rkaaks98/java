3e3b8aded27b53353214ceddf325b7a3a3a80ed9
wnstj050505 <wnstj050505@gmail.com>
2025-01-03 17:09:46 +0900

    131p

---
+++ ThisJava/src/ch04/sec05/PrintFrom.java
@@ -0,0 +1,16 @@
+package ch04.sec05;
+
+/*
+ * 김준서 교재 131
+ */
+
+public class PrintFrom {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i<=10) {
+			System.out.print(i + " ");
+			i++;
+		}
+	}
+
+}
