6d5e6a4f0a1c26dd8f7253481747ee94854a8191
wnstj050505 <wnstj050505@gmail.com>
2025-01-03 17:06:36 +0900

    130p

---
+++ ThisJava/src/ch04/sec04/Multiplication.java
@@ -0,0 +1,17 @@
+package ch04.sec04;
+
+/*
+ * 김준서 교재 130
+ */
+
+public class Multiplication {
+	public static void main(String[] args) {
+		for (int m=2; m<=9; m++) {
+			System.out.println("*** " + m + "단 ***");
+			for (int n=1; n<=9; n++) {
+				System.out.println(m+ " x " + n + " = " + (m*n));
+			}
+		}
+	}
+
+}
