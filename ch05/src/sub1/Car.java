3dab1f8b80c24cb019b1998c2bde2a3912836061
wnstj050505 <wnstj050505@gmail.com>
2025-01-07 11:25:12 +0900

    add ch05

---
+++ ch05/src/sub1/Car.java
@@ -0,0 +1,33 @@
+package sub1;
+/*
+ * 날짜 : 2025/01/07
+ * 이름 : 김준서
+ * 내용 : Java 클래스 실습하기
+ * Car 클래스 정의 (설계)
+ */
+
+public class Car {
+	
+	//속성(멤버 변수)
+	String name;
+	String color;
+	int speed;
+	
+	//기능(멤버 메서드)
+	
+	public void speedUp(int speed) {
+		// 속성 speed에 매개변수값 대입
+		this.speed += speed;
+	}
+	
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+	
+	public void show() {
+		
+		System.out.println("차량명 : " + this.name);
+		System.out.println("차량색 : " + this.color);
+		System.out.println("현재속도 : " + this.speed);
+	}
+}
