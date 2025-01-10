bd6633fb7835ac226daed1ad8cba452690eb7616
wnstj050505 <wnstj050505@gmail.com>
2025-01-10 12:41:08 +0900

    ch06

--- ThisJava/src/ch06/sec08/exam02/Computer.java
+++ ThisJava/src/ch06/sec08/exam02/Computer.java
@@ -6,6 +6,14 @@ public class Computer {
 	int sum(int ... values) {
 		//sum 변수 선언
 		int sum = 0;
+		
+		//values는 배열 타입의 변수처럼 사용
+		for(int i=0; i<values.length; i++) {
+			sum += values[i];
+		}
+		
+		//합산 결과를 리턴
+		return sum;
 	}
 	
 	
