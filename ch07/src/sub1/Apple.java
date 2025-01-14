f920f8ff131216cdedc1e295dac55d449258bb46
wnstj050505 <wnstj050505@gmail.com>
2025-01-14 12:38:25 +0900

    add ch07

---
+++ ch07/src/sub1/Apple.java
@@ -0,0 +1,28 @@
+package sub1;
+
+/*
+ * 날짜 : 2025/01/14
+ * 이름 : 김준서
+ * 내용 : Java Object 클래스 실습하기
+ */
+
+public class Apple { // 암묵적으로Object 클래스를 상속
+	
+	private String country;
+	private int price;
+	
+	public Apple(String country, int price) {
+		this.country = country;
+		this.price = price;		
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [country=" + country + ", price=" + price + "]";
+	}
+
+	
+	
+	
+}
+
