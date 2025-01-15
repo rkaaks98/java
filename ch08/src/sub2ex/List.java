b828a27cc28f193f0b26705b2fde8295522ce37d
wnstj050505 <wnstj050505@gmail.com>
2025-01-15 15:47:48 +0900

    add ch08

---
+++ ch08/src/sub2ex/List.java
@@ -0,0 +1,20 @@
+package sub2ex;
+
+import java.util.ArrayList;
+
+public class List {
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println(list);
+	}
+
+}
