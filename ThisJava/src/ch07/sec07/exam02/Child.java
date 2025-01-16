be1934e73e2fba352de08bbb9bd8e9b5e384e916
wnstj050505 <wnstj050505@gmail.com>
2025-01-16 17:47:37 +0900

    ch07

---
+++ ThisJava/src/ch07/sec07/exam02/Child.java
@@ -0,0 +1,19 @@
+package ch07.sec07.exam02;
+
+public class Child extends Parent {
+
+	//메소드 오버라이딩
+	
+	@Override
+	public void method2() {
+		System.out.println("Parent-method2()");
+	}
+
+
+	public void method3() {
+		System.out.println("Parent-method3()");
+	}
+	
+	
+
+}
