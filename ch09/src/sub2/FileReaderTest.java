d340ae0395d2d21f33f496ffe8de818b5806f1e8
wnstj050505 <wnstj050505@gmail.com>
2025-01-16 11:46:15 +0900

    add ch09

---
+++ ch09/src/sub2/FileReaderTest.java
@@ -0,0 +1,67 @@
+package sub2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * 날짜 : 2025/01/16
+ * 이름 : 김준서
+ * 내용 : Java 파일 입출력 문자 스트림 실습하기
+ */
+
+public class FileReaderTest {
+	
+	public static void main(String[] args) {
+		
+		
+		String source = "C:\\Users\\lotte6\\Desktop\\Test1.txt";
+		String target = "C:\\Users\\lotte6\\Desktop\\Test2.txt";
+		
+		
+		//스트림 생성
+		try {
+			FileReader fr = new FileReader(source);	//입력 스트림
+			FileWriter fw = new FileWriter(target);//출력 스트림
+			
+			//스트림 데이터 전송
+			while(true) {
+				
+				//입력스트림으로 파일 데이터 읽기
+				int data = fr.read();
+				
+				if(data == -1) {
+					//파일 데이터가 없을때
+					break;	
+				}
+				
+				//숫자를 문자로 변환
+				char ch = (char) data;
+				System.out.print(ch);
+				
+				//출력스트림으로 문자 데이터 쓰기
+				fw.write(data);
+			}
+			
+			//스트림 해제
+			fr.close();
+			fw.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 종료...");
+		
+		
+		
+	}//main end
+
+}
---
+++ ch09/src/sub3/FileTransferTest.java
@@ -0,0 +1,52 @@
+package sub3;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+ * 날짜 : 2025/01/16
+ * 이름 : 김준서
+ * 내용 : Java 파일 입출력 버퍼 스트림 실습하기
+ */
+
+public class FileTransferTest {
+	public static void main(String[] args) {
+		
+		
+		String source = "C:\\Users\\lotte6\\Desktop\\Sample.tif";
+		String target = "C:\\Users\\lotte6\\Desktop\\Sample2.tif";
+		
+		
+		//스트림 생성
+		try {
+			FileInputStream fis = new FileInputStream(source);	//입력 스트림
+			FileOutputStream fos = new FileOutputStream(target);//출력 스트림
+			
+			
+			//파일복사
+			fis.transferTo(fos);
+			
+			
+			//스트림 해제
+			fis.close();
+			fos.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 종료...");
+		
+		
+		
+	}//main end
+
+}
