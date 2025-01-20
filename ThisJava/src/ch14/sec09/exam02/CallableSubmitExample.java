6e14c815b911bf50e0861249f27ac79578fefc79
wnstj050505 <wnstj050505@gmail.com>
2025-01-20 14:10:14 +0900

    ch14

---
+++ ThisJava/src/ch14/sec09/exam02/CallableSubmitExample.java
@@ -0,0 +1,20 @@
+package ch14.sec09.exam02;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//633
+
+public class CallableSubmitExample {
+
+	public static void main(String[] args) {
+		
+		//ExecutorService 생성
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		
+		//계산 작업 생성 및 처리 요청
+		
+		for(int i=1;)
+	}
+}
