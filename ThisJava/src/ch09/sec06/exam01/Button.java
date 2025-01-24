5de24a63364905a2c37835717fbd847621e0afd8
wnstj050505 <wnstj050505@gmail.com>
2025-01-24 17:42:44 +0900

    ch09 git commit -m ch09

--- ThisJava/src/ch09/sec06/exam01/Button.java
+++ ThisJava/src/ch09/sec06/exam01/Button.java
@@ -4,7 +4,15 @@ public class Button {
 	//정적 중첩 인터페이스
 	public static interface ClickListener {
 		//추상 메소드
-		void onClick();
+		void onClick();}
+	
+	//필드
+	private ClickListener clickListener;
+	
+	//메소드
+	
+	public void secClickListener(ClickListener clickListener) {
+		this.clickListener = clickListener;
 	}
 
 }
