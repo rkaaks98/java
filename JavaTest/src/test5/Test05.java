2dcebc8da45f24f8585a5fc7dcfa48fd74714c56
wnstj050505 <wnstj050505@gmail.com>
2025-01-27 15:33:31 +0900

    test5

---
+++ JavaTest/src/test5/Test05.java
@@ -0,0 +1,26 @@
+package test5;
+
+public class Test05 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		boolean sign = false;
+		double pi = 0;
+		
+		for(int i=1; i<=10000000; i+=2) {
+			
+			if(!sign) {
+				pi += 1.0 / i;
+				sign = true;
+			}else {
+				pi-= 1.0 / i;
+				sign = false;
+				
+			}
+			
+			System.out.printf("i = %d, PI = %f\n", i, 4 * pi);
+		}
+	}
+}
